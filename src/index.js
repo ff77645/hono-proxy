c036e63381df85d3d700bdc7f392a8235ed4d7df
fy <fy77645@126.com>
2024-10-08 16:43:57 +0800

    feat

--- src/index.js
+++ src/index.js
@@ -3,67 +3,51 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 // 定义缓存存储
-const cache = caches.default
 
 app.get('/', (c) => c.json({
-	url:c.req.url
+  url:c.req.url
 }))
 
+const cache = caches.default
 
-// 代理并缓存请求
-app.all('/proxy/*', async (c) => {
-  // 从 URL 路径中获取目标地址
+app.get('/proxy/*',async (c)=>{
   const targetUrl = c.req.url.replace(c.req.url.split('/proxy/')[0] + '/proxy/', '')
-
-	if (c.req.method === 'GET') {
-		// 检查缓存是否已有响应
-		const parsedUrl = new URL(targetUrl)
-		const cacheKey = new Request(parsedUrl.origin + parsedUrl.pathname)
-		const cachedResponse = await cache.match(cacheKey)
-		if (cachedResponse) {
-			// 记录读取缓存次数
-			const kv = c.env.kvdb
-			const _key = `cached_${parsedUrl.pathname.replace(/\//g,'_')}`
-			const proxyTime = await kv.get(_key) || '0'
-			const newProxyTime = parseInt(proxyTime) + 1
-			await kv.put(_key, newProxyTime.toString())
-			// 如果缓存命中，返回缓存的响应
-			return cachedResponse
-		}
-	}
-
-  try {
-    // 向目标地址发起请求
-    const response = await fetch(targetUrl, {
-      method: c.req.method,
-      headers: c.req.headers,
-      body: c.req.body,
+  const {origin,pathname} = new URL(targetUrl)
+  const cacheKey = new Request(origin+pathname)
+  console.log('cacheKey',cacheKey.url,cacheKey.method);
+  
+  const cachedResponse = await cache.match(cacheKey) 
+  
+  if(cachedResponse) {
+    console.log('读取缓存: ',targetUrl);
+    return new Response(cachedResponse.body,{
+      status: cachedResponse.status,
+      headers: {
+        ...cachedResponse.headers,
+        'use-cache':'true'
+      },
     })
-
-	// 如果是 GET 请求，克隆响应，保存一份到缓存中
-		if (c.req.method === 'GET') {
-			const parsedUrl = new URL(targetUrl)
-			const cacheKey = new Request(parsedUrl.origin + parsedUrl.pathname)
-      const responseClone = response.clone()
-      await cache.put(cacheKey, responseClone)
-
-			// 记录代理次数
-			const kv = c.env.kvdb
-			const _key = `proxy_${parsedUrl.pathname.replace(/\//g,'_')}`
-			const proxyTime = await kv.get(_key) || '0'
-			const newProxyTime = parseInt(proxyTime) + 1
-			await kv.put(_key, newProxyTime.toString())
-    }
-
-    // 返回目标地址的响应
-    return new Response(response.body, {
-      status: response.status,
-      headers: response.headers,
-    })
-  } catch (error) {
-    return c.text('Failed to proxy request', 500)
   }
+  
+  // 向目标地址发起请求
+  const response = await fetch(targetUrl, {
+    method: c.req.method,
+    headers: c.req.headers,
+    body: c.req.body,
+  })
+
+  // 缓存响应
+  const res = new Response(response.body, {
+    status: response.status,
+    // headers: response.headers,
+  })
+
+  console.log('写入缓存: ',targetUrl);
+  await cache.put(cacheKey, res.clone())
+  
+  return res
 })
+
 
 // 清除指定路径的缓存
 app.delete('/cache-clear', async (c) => {
@@ -75,19 +59,46 @@ app.delete('/cache-clear', async (c) => {
 
   try {
     // 构造对应路径的缓存请求对象
-		const parsedUrl = new URL(targetUrl)
-		const cacheKey = new Request(parsedUrl.origin + parsedUrl.pathname)
+		const {origin,pathname} = new URL(targetUrl)
+		const cacheKey = new Request(origin+pathname)
+    console.log('cacheKey',cacheKey.url,cacheKey.method);
     // 删除缓存中与该路径匹配的项
     const cacheDeleted = await cache.delete(cacheKey)
+    console.log('清除缓存: ',targetUrl,cacheDeleted);
+    
     if (cacheDeleted) {
-      return c.text(`Cache for ${targetUrl} cleared successfully`, 200)
+      return c.text(`Cache for [${targetUrl}] cleared successfully`, 200)
     } else {
-      return c.text(`No cache found for ${targetUrl}`, 404)
+      return c.text(`No cache found for [${targetUrl}]`, 404)
     }
   } catch (error) {
     return c.text('Failed to clear cache', 500)
   }
 })
+
+
+// 代理并缓存请求
+app.all('/proxy/*', async (c) => {
+  const targetUrl = c.req.url.replace(c.req.url.split('/proxy/')[0] + '/proxy/', '')
+  try {
+    // 向目标地址发起请求
+    const response = await fetch(targetUrl, {
+      method: c.req.method,
+      headers: c.req.headers,
+      body: c.req.body,
+    })
+
+    // 返回目标地址的响应
+    return new Response(response.body, {
+      status: response.status,
+      headers: response.headers,
+    })
+  } catch (error) {
+    console.error('请求失败: ',c.req.method,targetUrl)
+    return c.text('Failed to proxy request', 500)
+  }
+})
+
 
 // 默认路由，处理所有未匹配的请求
 app.all('*', (c) => c.text('Route not found', 404))
